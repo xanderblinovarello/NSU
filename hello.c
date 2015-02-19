de38c5dc284d6d6ff78df875654cddc4d2f53df3
xanderblinovarello <xanderblinov@arello-mobile.com>
2015-02-19 17:17:08 +0600

    initia

---
+++ hello.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+void f()
+{
+	printf("%s\n", "func");
+}
+
+int main()
+{
+
+	int d;
+	scanf("%d",&d);
+	//hello world bla
+	/*
+	printf("%s\n", "hello");
+	printf("%s\n", "hello");
+	printf("%s\n", "hello");
+	printf("%s\n", "hello");
+	*/
+	f();
+	printf("%s\n %d", "hello", d);
+	return 0;
+}
+
+
